7795c00670936b184eebde074b0e617a51eddc8c
Lucas Bechberger <lbechberger@uos.de>
2018-01-30 15:12:50 +0100

    additional evaluation metrics

--- ltn_code/util.py
+++ ltn_code/util.py
@@ -7,6 +7,7 @@ Created on Mon Dec  4 12:19:52 2017
 @author: lbechberger
 """
 import ConfigParser
+from math import log
 
 def parse_config_file(config_file_name, config_name):
     """Extracts all parameters of interest form the given config file."""
@@ -178,6 +179,54 @@ def average_precision(predictions, vectors):
     average_precision = (1.0 * summed_precision) / count
     return average_precision
 
+def exact_match_prefix(predictions, vectors):
+    """Computes the exact match for the given vectors and the given predictions by looking at the highest ranked predictions.
+    
+    If there are n ground truth labels, check whether the first n predictions are correct. The higher, the better."""
+    
+    count = 0
+    idx = 0
+    for (true_labels, vector) in vectors:
+        n_labels = len(true_labels)
+        filtered_predictions = []
+        for label, memberships in predictions.iteritems():
+            filtered_predictions.append((label, memberships[idx]))
+        filtered_predictions.sort(key = lambda x: x[1], reverse = True) # sort in descending order based on membership
+        filtered_predictions = list(map(lambda x: x[0], filtered_predictions))[:n_labels]
+        
+        equivalent = True
+        for pred in filtered_predictions:
+            if pred not in true_labels:
+                equivalent = False
+                break
+        if equivalent:
+            count += 1
+        idx += 1
+    
+    exact_match_prefix = (1.0 * count) / len(vectors)
+    return exact_match_prefix
+
+def cross_entropy_loss(predictions, vectors):
+    """Computes the cross entropy loss between the predictions and the ground truth labels.
+    
+    How close are the numeric scores to the values of 0 and 1? The lower, the better."""
+    
+    sum_of_cross_entropies = 0
+    idx = 0
+    for (true_labels, vector) in vectors:
+        
+        for label in true_labels:
+            if predictions[label][idx] == 0:
+                sum_of_cross_entropies -= 1000
+            else:
+                sum_of_cross_entropies += log(predictions[label][idx])
+        
+        idx += 1
+    
+    return (-1.0 * sum_of_cross_entropies) / len(vectors)
+    
+
+
 def distinct_label_set(vectors):
     """Computes the distinct label set of the given data set.
     
@@ -236,6 +285,8 @@ def evaluate(train_predictions, train_vectors, validation_predictions, validatio
     print("Coverage on training data: {0}".format(coverage(train_predictions, train_vectors)))
     print("Ranking loss on training data: {0}".format(ranking_loss(train_predictions, train_vectors, all_labels)))
     print("Average precision on training data: {0}".format(average_precision(train_predictions, train_vectors)))
+    print("Exact match prefix: {0}".format(exact_match_prefix(train_predictions, train_vectors)))
+    print("Cross entropy loss: {0}".format(cross_entropy_loss(train_predictions, train_vectors)))
     
     # validation data (the stuff that matters)
     print(" ")
@@ -256,4 +307,6 @@ def evaluate(train_predictions, train_vectors, validation_predictions, validatio
     print("Coverage on validation data: {0}".format(coverage(validation_predictions, validation_vectors)))
     print("Ranking loss on validation data: {0}".format(ranking_loss(validation_predictions, validation_vectors, all_labels)))
     print("Average precision on validation data: {0}".format(average_precision(validation_predictions, validation_vectors)))
+    print("Exact match prefix: {0}".format(exact_match_prefix(validation_predictions, validation_vectors)))
+    print("Cross entropy loss: {0}".format(cross_entropy_loss(validation_predictions, validation_vectors)))
     
