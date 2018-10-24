ee48021ec093efc80fd882368e505c4a2f2a9aad
Lucas Bechberger <lbechberger@uos.de>
2018-10-24 13:25:37 +0200

    cleaning code, fixing issue of div by 0

--- ltn_code/run_counting.py
+++ ltn_code/run_counting.py
@@ -64,18 +64,24 @@ for first_concept in config["concepts"]:
                     if second_concept in labels:
                         count_np[count_idx] += 1
         
-        # division and subtraction are taking place element-wise thanks to numpy            
-        pos_pos_implication = count_pp / count_p
-        pos_neg_implication = 1 - pos_pos_implication
-        neg_pos_implication = count_np / count_n
-        neg_neg_implication = 1 - neg_pos_implication
-        jaccard_distance = 1 - (count_pp / (count_p + count_np))
+        # division and subtraction are taking place element-wise thanks to numpy  
+        if all(x > 0 for x in count_p):
+            p_impl_p = count_pp / count_p
+            p_impl_n = 1 - p_impl_p
+            
+            rules['pIMPLp'].append([p_impl_p, first_concept, second_concept])        
+            rules['pIMPLn'].append([p_impl_n, first_concept, second_concept])        
 
-        rules['pIMPLp'].append([pos_pos_implication, first_concept, second_concept])        
-        rules['pIMPLn'].append([pos_neg_implication, first_concept, second_concept])        
-        rules['nIMPLp'].append([neg_pos_implication, first_concept, second_concept])        
-        rules['nIMPLn'].append([neg_neg_implication, first_concept, second_concept])     
-        rules['pDIFFp'].append([jaccard_distance, first_concept, second_concept])
+        if all(x > 0 for x in count_n):
+            n_impl_p = count_np / count_n
+            n_impl_n = 1 - n_impl_p
+
+            rules['nIMPLn'].append([n_impl_n, first_concept, second_concept])     
+            rules['nIMPLp'].append([n_impl_p, first_concept, second_concept])        
+
+        if all(x > 0 for x in (count_p + count_np)):
+            jaccard_distance = 1 - (count_pp / (count_p + count_np))
+            rules['pDIFFp'].append([jaccard_distance, first_concept, second_concept])
 
 # now look for rules involving three concepts
 # like "first_concept AND second_concept IMPLIES third_concept" or "first_concept IMPLIES second_concept OR third_concept"
@@ -134,56 +140,66 @@ for first_concept in config["concepts"]:
                             else:
                                 count_nnn[count_idx] += 1
             
-            # take care of "A AND B IMPLIES C"            
-            p_and_p_impl_p = count_ppp / count_pp
-            p_and_p_impl_n = count_ppn / count_pp
-            p_and_n_impl_p = count_pnp / count_pn
-            p_and_n_impl_n = count_pnn / count_pn
+            # take care of "A AND B IMPLIES C"  
+            if all(x > 0 for x in count_pp):
+                p_and_p_impl_p = count_ppp / count_pp
+                p_and_p_impl_n = count_ppn / count_pp
+
+                rules['pANDpIMPLp'].append([p_and_p_impl_p, first_concept, second_concept, third_concept])
+                rules['pANDpIMPLn'].append([p_and_p_impl_n, first_concept, second_concept, third_concept])
+
+            if all(x > 0 for x in count_pn):
+                p_and_n_impl_p = count_pnp / count_pn
+                p_and_n_impl_n = count_pnn / count_pn
             
-            n_and_p_impl_p = count_npp / count_np
-            n_and_p_impl_n = count_npn / count_np
-            n_and_n_impl_p = count_nnp / count_nn
-            n_and_n_impl_n = count_nnn / count_nn
-            
-            rules['pANDpIMPLp'].append([p_and_p_impl_p, first_concept, second_concept, third_concept])
-            rules['pANDpIMPLn'].append([p_and_p_impl_n, first_concept, second_concept, third_concept])
-            rules['pANDnIMPLp'].append([p_and_n_impl_p, first_concept, second_concept, third_concept])
-            rules['pANDnIMPLn'].append([p_and_n_impl_n, first_concept, second_concept, third_concept])
-            
-            rules['nANDpIMPLp'].append([n_and_p_impl_p, first_concept, second_concept, third_concept])
-            rules['nANDpIMPLn'].append([n_and_p_impl_n, first_concept, second_concept, third_concept])
-            rules['nANDnIMPLp'].append([n_and_n_impl_p, first_concept, second_concept, third_concept])
-            rules['nANDnIMPLn'].append([n_and_n_impl_n, first_concept, second_concept, third_concept])
+                rules['pANDnIMPLp'].append([p_and_n_impl_p, first_concept, second_concept, third_concept])
+                rules['pANDnIMPLn'].append([p_and_n_impl_n, first_concept, second_concept, third_concept])
+
+            if all(x > 0 for x in count_np):
+                n_and_p_impl_p = count_npp / count_np
+                n_and_p_impl_n = count_npn / count_np
+
+                rules['nANDpIMPLp'].append([n_and_p_impl_p, first_concept, second_concept, third_concept])
+                rules['nANDpIMPLn'].append([n_and_p_impl_n, first_concept, second_concept, third_concept])
+
+            if all(x > 0 for x in count_nn):
+                n_and_n_impl_p = count_nnp / count_nn
+                n_and_n_impl_n = count_nnn / count_nn
+                       
+                rules['nANDnIMPLp'].append([n_and_n_impl_p, first_concept, second_concept, third_concept])
+                rules['nANDnIMPLn'].append([n_and_n_impl_n, first_concept, second_concept, third_concept])
             
             # take care of "A IMPLIES B OR C"
-            p_impl_p_or_p = (count_pp + count_pnp) / count_p    # size(B or C) = size(B) + size(not B and C) 
-            p_impl_p_or_n = (count_pp + count_pnn) / count_p    # size(B or not C) = size(B) + size(not B and not C)
-            p_impl_n_or_p = (count_pn + count_ppp) / count_p    # size(not B or C) = size(not B) + size(B and C)
-            p_impl_n_or_n = (count_pn + count_ppn) / count_p    # size(not B or not C) = size(not B) + size(B and not C)
+            if all(x > 0 for x in count_p):
+                p_impl_p_or_p = (count_pp + count_pnp) / count_p    # size(B or C) = size(B) + size(not B and C) 
+                p_impl_p_or_n = (count_pp + count_pnn) / count_p    # size(B or not C) = size(B) + size(not B and not C)
+                p_impl_n_or_p = (count_pn + count_ppp) / count_p    # size(not B or C) = size(not B) + size(B and C)
+                p_impl_n_or_n = (count_pn + count_ppn) / count_p    # size(not B or not C) = size(not B) + size(B and not C)
             
-            n_impl_p_or_p = (count_np + count_nnp) / count_n    # size(B or C) = size(B) + size(not B and C) 
-            n_impl_p_or_n = (count_np + count_nnn) / count_n    # size(B or not C) = size(B) + size(not B and not C)
-            n_impl_n_or_p = (count_nn + count_npp) / count_n    # size(not B or C) = size(not B) + size(B and C)
-            n_impl_n_or_n = (count_nn + count_npn) / count_n    # size(not B or not C) = size(not B) + size(B and not C)
+                rules['pIMPLpORp'].append([p_impl_p_or_p, first_concept, second_concept, third_concept])
+                rules['pIMPLpORn'].append([p_impl_p_or_n, first_concept, second_concept, third_concept])
+                rules['pIMPLnORp'].append([p_impl_n_or_p, first_concept, second_concept, third_concept])
+                rules['pIMPLnORn'].append([p_impl_n_or_n, first_concept, second_concept, third_concept])
             
-            rules['pIMPLpORp'].append([p_impl_p_or_p, first_concept, second_concept, third_concept])
-            rules['pIMPLpORn'].append([p_impl_p_or_n, first_concept, second_concept, third_concept])
-            rules['pIMPLnORp'].append([p_impl_n_or_p, first_concept, second_concept, third_concept])
-            rules['pIMPLnORn'].append([p_impl_n_or_n, first_concept, second_concept, third_concept])
+            if all(x > 0 for x in count_n):
+                n_impl_p_or_p = (count_np + count_nnp) / count_n    # size(B or C) = size(B) + size(not B and C) 
+                n_impl_p_or_n = (count_np + count_nnn) / count_n    # size(B or not C) = size(B) + size(not B and not C)
+                n_impl_n_or_p = (count_nn + count_npp) / count_n    # size(not B or C) = size(not B) + size(B and C)
+                n_impl_n_or_n = (count_nn + count_npn) / count_n    # size(not B or not C) = size(not B) + size(B and not C)
             
-            rules['nIMPLpORp'].append([n_impl_p_or_p, first_concept, second_concept, third_concept])
-            rules['nIMPLpORn'].append([n_impl_p_or_n, first_concept, second_concept, third_concept])
-            rules['nIMPLnORp'].append([n_impl_n_or_p, first_concept, second_concept, third_concept])
-            rules['nIMPLnORn'].append([n_impl_n_or_n, first_concept, second_concept, third_concept])
+                rules['nIMPLpORp'].append([n_impl_p_or_p, first_concept, second_concept, third_concept])
+                rules['nIMPLpORn'].append([n_impl_p_or_n, first_concept, second_concept, third_concept])
+                rules['nIMPLnORp'].append([n_impl_n_or_p, first_concept, second_concept, third_concept])
+                rules['nIMPLnORn'].append([n_impl_n_or_n, first_concept, second_concept, third_concept])
 
 
 for rule_type, rule_instances in rules.items():
     for confidence_threshold in [0.7, 0.8, 0.9, 0.95, 0.99]:
-        filtered_list = list(filter(lambda x: x[0][1] >= confidence_threshold, rule_instances))
+        filtered_list = list(filter(lambda x: x[0][1] >= confidence_threshold and x[0][0] >= confidence_threshold, rule_instances))
         if len(filtered_list) == 0:
             print("Could not find rules of type {0} when trying to achieve {1} on validation set.".format(rule_type, confidence_threshold))
             continue
-        training_threshold = min(map(lambda x: x[0][0], filtered_list), confidence_threshold)
+        training_threshold = min(map(lambda x: x[0][0], filtered_list))
         average_test_accuracy = sum(map(lambda x: x[0][2], filtered_list)) / len(filtered_list)
         minimal_test_accuracy = min(map(lambda x: x[0][2], filtered_list))
         print("Threshold for rules of type {0} when trying to achieve {1} on validation set: {2} " \
